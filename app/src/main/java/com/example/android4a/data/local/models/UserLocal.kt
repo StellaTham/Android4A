7f7b067c532a611f4802f01066d4423b70b7e949
StellaTham <stella.thammavong@gmail.com>
2020-11-18 23:34:56 +0100

    Clean Architecture + BDD

---
+++ app/src/main/java/com/example/android4a/data/local/models/UserLocal.kt
@@ -0,0 +1,24 @@
+package com.example.android4a.data.local.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android4a.domain.entity.User
+
+@Entity
+data class UserLocal(
+    @PrimaryKey(autoGenerate = true) var uid: Int? = null,
+    @ColumnInfo(name = "email") val email: String
+)
+
+fun User.toData(): UserLocal{
+    return UserLocal(
+        email = this.email
+    )
+}
+
+fun UserLocal.toEntity(): User{
+    return User(
+        email = this.email
+    )
+}
